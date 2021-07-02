3688102f952da0633e6009353df9a4aef64fa7b0
lipingh <lipingh@andrew.cmu.edu>
2021-07-01 18:20:49 -0700

    import react

--- src/Game.js
+++ src/Game.js
@@ -1,23 +1,7 @@
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
+import React from 'react';
+import calculateWinner from './calculateWinner.js';
+import Board from './Board.js';
 
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
 class Game extends React.Component {
   constructor(props) {
     super(props);
